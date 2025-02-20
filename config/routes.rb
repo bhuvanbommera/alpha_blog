54eecb2c56ca1d6f85d6dcf7f86683d8d7eecd31
Bhuvan Bommera <bhuvanbommera5303@gmail.com>
2025-02-20 13:06:46 -0500

    Database modified and created articles view file to display

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   root 'pages#home'
   get 'about', to: 'pages#about'
+  resources :articles , only: [:show, :index]
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
--- db/migrate/20250218165846_show_date_time.rb
+++ db/migrate/20250218165846_show_date_time.rb
@@ -2,6 +2,6 @@ class ShowDateTime < ActiveRecord::Migration[8.0]
   def change
     
     
-    add_column :articles, :rating, :integer
+    
   end
 end
