2c58bb265e25c465dfffd24434615ab32028c636
Bhuvan Bommera <bhuvanbommera5303@gmail.com>
2025-03-05 13:56:55 -0500

    Added edit page to users

--- app/controllers/concerns/users_controller.rb
+++ app/controllers/concerns/users_controller.rb
@@ -3,6 +3,21 @@ def new
   @user = User.new
   #render plain: "This is the new user form."
 end
+
+def edit
+ @user = User.find(params[:id])
+end
+
+def update
+  @user = User.find(params[:id])
+  if @user.update(user_params)
+    flash[:notice]= "Your account was sucessfully updated"
+    redirect_to articles_path
+ else
+  render 'edit'
+ end
+end
+
 def create
   @user = User.new(user_params)
   if @user.save
@@ -13,7 +28,9 @@ def create
   end
 end
 end
+
 private
+
 def user_params
  params.require(:user).permit(:username, :email, :password)
 end
